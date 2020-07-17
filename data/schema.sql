2626711d2c9a57df56d3596fce597b3ef7c98191
ahmad-swedani <swedani390@gmail.com>
2020-07-17 19:26:08 +0300

    done lab-12

--- data/schema.sql
+++ data/schema.sql
@@ -7,4 +7,4 @@ CREATE TABLE IF NOT EXISTS BOOK_LIST(
     bookImage VARCHAR(255),
     bookCat VARCHAR(255)
     );
-    INSERT INTO BOOK_LIST(bookName,bookAuthor,bookDesc,bookImage,bookCat) VALUES('what if','Ahmad Swedani','to know result what if you do some thing at that time','https://i.pinimg.com/originals/60/7b/98/607b98c81bfc9f55fbfe3580759afd92.jpg','psychic');
+    -- INSERT INTO BOOK_LIST(bookName,bookAuthor,bookDesc,bookImage,bookCat) VALUES('what if','Ahmad Swedani','to know result what if you do some thing at that time','https://i.pinimg.com/originals/60/7b/98/607b98c81bfc9f55fbfe3580759afd92.jpg','psychic');
