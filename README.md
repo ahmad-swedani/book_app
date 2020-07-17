dc4146ffed3bb2992208739843ef2c9f4e9d6c64
ahmad-swedani <swedani390@gmail.com>
2020-07-17 18:40:47 +0300

    done lab-12

---
+++ README.md
@@ -0,0 +1,35 @@
+[go to The book_app](index.html)
+### About this repostory
+
+```markdown
+---
+The book_app: this is lab-12 in code301 with ASAC.LTUC .
+it is a ( book_app && a alot of books)
+---
+```
+
+### date
+
+Date published
+```markdown
+---
+date: 2020-07-15 05:40:00
+---
+```
+
+### author
+
+Author name
+
+```markdown
+---
+author: Ahmad Swedani
+---
+```
+### location
+
+```markdown
+---
+location: Amman-Jordan
+---
+```
---
+++ data/schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS BOOK_LIST;
+CREATE TABLE IF NOT EXISTS BOOK_LIST(
+    ID SERIAL PRIMARY KEY,
+    bookName VARCHAR(255),
+    bookAuthor VARCHAR(255),
+    bookDesc TEXT,
+    bookImage VARCHAR(255),
+    bookCat VARCHAR(255)
+    );
+    INSERT INTO BOOK_LIST(bookName,bookAuthor,bookDesc,bookImage,bookCat) VALUES('what if','Ahmad Swedani','to know result what if you do some thing at that time','https://i.pinimg.com/originals/60/7b/98/607b98c81bfc9f55fbfe3580759afd92.jpg','psychic');
